9abce9a3f3a66f55e105cbc456a71180252fecd4
Essence Alexander <upperlefty@zoho.eu>
2018-07-10 13:11:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ class App extends React.Component {
          header: "Header from props...",
          content: "Content from props..."
       }
+ }
+   render() {
+      return (
+         <div>
+            <Header headerProp = {this.state.header}/>
+            <Content contentProp = {this.state.content}/>
+         </div>
+      );
+   }
+}
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>{this.props.headerProp}</h1>
+         </div>
+      );
+   }
+}
