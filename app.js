91631a8fc36597b4dcca42da8bb939a6405f9d4b
Essence Alexander <upperlefty@zoho.eu>
2018-07-10 13:12:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,3 +28,11 @@ class Header extends React.Component {
 }
 class Content extends React.Component {
    render() {
+return (
+         <div>
+            <h2>{this.props.contentProp}</h2>
+         </div>
+      );
+   }
+}
+export default App;
